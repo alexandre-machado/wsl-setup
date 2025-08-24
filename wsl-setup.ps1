ba725bd88b456476e68bd3df39d10c71a5aaf859
Alexandre Machado <alexandre@machado.cc>
2025-08-24 17:13:33 -0300

    feat: improve Git variable prompts in WSL setup script and set defaults

--- wsl-setup.ps1
+++ wsl-setup.ps1
@@ -6,10 +6,16 @@ wsl --set-default Ubuntu-24.04
 write-host "1/2 WSL on Windows is installed." -ForegroundColor Green
 
 # Prompt for Git variables
-$GIT_NAME = Read-Host "Enter your name for Git"
-$GIT_EMAIL = Read-Host "Enter your email for Git"
+$defaultGitName = "Alexandre Machado"
+$defaultGitEmail = "alexandre@machado.cc"
 
-wsl ~ -e bash -c "echo 'Starting WSL setup...' && \
+$GIT_NAME = Read-Host "Enter your name for Git [$defaultGitName]"
+if ([string]::IsNullOrWhiteSpace($GIT_NAME)) { $GIT_NAME = $defaultGitName }
+
+$GIT_EMAIL = Read-Host "Enter your email for Git [$defaultGitEmail]"
+if ([string]::IsNullOrWhiteSpace($GIT_EMAIL)) { $GIT_EMAIL = $defaultGitEmail }
+
+wsl ~ -e bash -lc "echo 'Starting WSL setup...' && \
 export GIT_NAME='$GIT_NAME' && \
 export GIT_EMAIL='$GIT_EMAIL' && \
 git clone https://github.com/alexandre-machado/wsl-setup.git && \
